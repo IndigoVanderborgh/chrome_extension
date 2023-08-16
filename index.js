a7c2dda523a6ff49cccd628300c33f86b840c706
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-15 18:56:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ function saveLead() {
     console.log("Button Clicked")
 }
 
+
+// Time to learn listeners! When the user clicks the button we want it to listen for that
+// click event, using the event listener method and invoke a function.
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
+
 // 1. Grab the box from the DOM and store it in a variable
 // 2. Add a click event listener to the box 
 // 3. Log out "I want to open the box!" when it's clicked
 
+let box = document.getElementById("box")
+
+box.addEventListener("click", function() {
+    console.log("I want to open the box")
+})
