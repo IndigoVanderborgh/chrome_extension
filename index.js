b06f5f334a0c8c72198c2ffe5d166db6a28e33f9
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-16 18:27:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,3 @@ let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked from addEventListener")
 })
-
-// 1. Grab the box from the DOM and store it in a variable
-// 2. Add a click event listener to the box 
-// 3. Log out "I want to open the box!" when it's clicked
-
-let box = document.getElementById("box")
-
-box.addEventListener("click", function() {
-    console.log("I want to open the box")
-})
