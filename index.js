ba6786e571928c265f767aefd956a5d0c4b6e07a
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-18 20:42:51 -0700

    13

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-El")
+
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
-
-
 
 // Log out the items in the myLeads array using a for loop 
 for (let i = 0; i < myLeads.length; i++) {
