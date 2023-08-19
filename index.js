d1e44f9fea939109f99bca123ac10ee188c1c8c6
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-18 20:21:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-
-// old function 
-// function saveLead() {
-//     console.log("Button Clicked")
-// }
-
-
-// Time to learn listeners! When the user clicks the button we want it to listen for that
-// click event, using the event listener method and invoke a function.
-
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
-
+let myLeads = []
+let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 
-
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-
-// inputBtn.addEventListener("click", function() {
-//     // Push the value from the inputEl into the myLeads array 
-//     // instead of the hard-coded "www.awesomeleads.com" value
-//     // Google -> "get value from input field javascript"
-//     myLeads.push("www.awesomelead.com")
-//     console.log("Button clicked from addEventListener")
-// })
-
-
 inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript"
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
-// in html we have id="input-btn". on line 15, we connect html's "input-btn" to inputBtn in JS
-// the element is used on line 17, where the first thing is inputBtn, connecting the 
-// element to the variable stated in line 15.
+// Log out the items in the myLeads array using a for loop 
 
-// Lets setup the infrastructure: an array and input variable
-
-let myLeads = []
-let inputEl = document.getElementById("input-el")
