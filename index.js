a117fa52765940615ea487f866591d4d67228564
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-21 19:47:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+// let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-El")
@@ -6,6 +6,8 @@ const ulEl = document.getElementById("ul-El")
 // localStorage.setItem("myLead", "www.examplelead.com")
 let name = localStorage.getItem("myName")
 console.log(name)
+
+// localStorage.clear()
 
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
@@ -17,7 +19,16 @@ console.log(name)
 // localStorage.clear()
 // PS: both key and value need to be strings
 
+let myLeads = `["www.awesomelead.com"]`
 
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
