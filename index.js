1d2c90364440e4c7d176077c39e2891a700ad471
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-21 19:27:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-El")
 
 // localStorage.setItem("myLead", "www.examplelead.com")
+let name = localStorage.getItem("myName")
+console.log(name)
+
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
 
 
 inputBtn.addEventListener("click", function() {
